565450bfb7464368306e3d20b961518bc838dd30
Naim AK <149870242+naimak659@users.noreply.github.com>
2025-04-07 16:42:04 +0600

    Refactor job API routes: improve error handling, ensure connection is established before fetching data, and enhance response messages for PUT and DELETE operations.

--- src/app/(routes)/api/jobs/[id]/route.ts
+++ src/app/(routes)/api/jobs/[id]/route.ts
@@ -14,7 +14,6 @@ export const GET = async (
   searchParams: Promise<{ [key: string]: string }>
 ) => {
   try {
-    await connect();
     const { id } = await context.params;
     const cachedJobs = cache.get(`${JOB_CACHE_KEY_PREFIX}${id}`);
     if (cachedJobs) {
@@ -24,7 +23,7 @@ export const GET = async (
         { status: 200 }
       );
     }
-
+    await connect();
     const ID = isValidObjectId(id)
       ? id
       : Types.ObjectId.createFromHexString(id);
@@ -38,7 +37,10 @@ export const GET = async (
     cache.set(`${JOB_CACHE_KEY_PREFIX}${id}`, job);
 
     return NextResponse.json(new ApiResponse(job, "successfully fetched"));
-  } catch (error) {}
+  } catch (error: any) {
+    console.log("error while geting job via id", error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
 };
 
 export const PUT = async (
@@ -50,19 +52,63 @@ export const PUT = async (
   try {
     await connect();
     const { id } = await context.params;
-    const cachedJobs = cache.get(`${JOB_CACHE_KEY_PREFIX}${id}`);
-    console.log("cachedJobs: ", cachedJobs);
-    // if(cachedJobs){
-    //   const data = findJobByIdFromCache(cachedJobs, id)
-    // }
+    const body = await req.json();
     const ID = isValidObjectId(id)
       ? id
       : Types.ObjectId.createFromHexString(id);
 
-    return NextResponse.json(new ApiResponse("", "successfully fetched"), {
-      status: 200,
+    const updatedJob = await Job.findByIdAndUpdate(ID, body, {
+      new: true,
     });
+
+    if (!updatedJob) {
+      return NextResponse.json(new ApiError("Job not found"), {
+        status: 404,
+      });
+    }
+    return NextResponse.json(
+      new ApiResponse(updatedJob, "successfully fetched"),
+      {
+        status: 200,
+      }
+    );
   } catch (error) {
     console.log("error while geting ", error);
+  }
+};
+
+export const DELETE = async (
+  req: NextRequest,
+  context: {
+    params: Promise<any>;
+  }
+) => {
+  try {
+    await connect();
+    const { id } = await context.params;
+    const ID = isValidObjectId(id)
+      ? id
+      : Types.ObjectId.createFromHexString(id);
+
+    const deletedJob = await Job.findByIdAndDelete(ID);
+    cache.del(`${JOB_CACHE_KEY_PREFIX}${id}`);
+    if (!deletedJob) {
+      return NextResponse.json(new ApiError("Job not found"), {
+        status: 404,
+      });
+    }
+
+    return NextResponse.json(
+      new ApiResponse(deletedJob, "successfully deleted"),
+      {
+        status: 200,
+      }
+    );
+  } catch (error: any) {
+    console.log("error while deleting job", error);
+    return NextResponse.json(
+      { message: "Something went wrong" },
+      { status: 500 }
+    );
   }
 };
